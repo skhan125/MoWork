5164db8d83c12ed034e24a3ae7743354fcaac832
Mohammad2421 <km78135@mail.harpercollege.edu>
2024-10-16 09:00:06 -0500

    Update Week 5 Factorial.cpp

--- Week 5 Factorial.cpp
+++ Week 5 Factorial.cpp
@@ -1,1 +1,19 @@
+//Factorial of a number 
+// 5! = 1 x 2 x 3 x 4 x 5 --> 120
+#include<iostream>
+using namespace std; 
+int main() 
+{
+    int num, fact = 1 ; 
+    cout << "Please enter the number for the factorial "; 
+    cin >> num; 
+    /*for (int i = 1; i <=num; i++)//incrementing for loop 
+    {
+}*/
+for (int i = num; i > 1; i --)
+{
+    fact *= i;// fact = fact * i; 
+}
+cout << "The factorial of  " << num << " is " << fact << end1; 
+return 0; 
 
