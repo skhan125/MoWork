d98a4a5ee8634e2a68ebd1c519ffa75e98db48f9
Mohammad2421 <km78135@mail.harpercollege.edu>
2024-10-21 22:13:55 -0500

    Update Week 5 NestedLoops.cpp

--- Week 5 NestedLoops.cpp
+++ Week 5 NestedLoops.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+    int num, fact;
+    char ur; // user response
+
+    cout << "Would you like to check the factorial of a number? (Y/N): ";
+    cin >> ur;
+
+    while ((ur == 'Y') || (ur == 'y')) { // pre-test
+        fact = 1; // re-initialization
+        cout << "\nPlease enter the number for the factorial: ";
+        cin >> num;
+
+        int i = 1;
+        while (i <= num) {
+            fact = fact * i;
+            i++;
+        }
+
+        cout << "The factorial of " << num << " is " << fact << endl;
+        cout << "\nWould you like to check the factorial of another number? (Y/N): ";
+        cin >> ur;
+    }
+
+    return 0;
+}
+
+FINISHED 
