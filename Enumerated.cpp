47ae4dd890679b139fea76a37a04f9be2b116304
skhan125 <112320594+skhan125@users.noreply.github.com>
2024-11-04 19:58:35 -0600

    Create Enumerated.cpp

---
+++ Enumerated.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    enum size { SMALL = 10; MEDIUM = 20, LARGE, EXTRA, SPECIAL);//defining domain
+    size mysize, yoursize, hersize, hissize;
+    mysize = MEDIUM;
+    cout << "My size is: " << mysize << endl;//1
+
+    yoursize = size (mysize + 2);//casting
+    cout << "Your size is: " << yoursize << endl;//3
+
+    hersize = size (MEDIUM + 1);
+    cout << "Her size is: " << hersize << endl;//3
+
+    hissize = size ((LARGE - MEDIUM) * 10);
+    cout << "His size is: " << hissize << endl;//1
+
+  return 0;
+}
